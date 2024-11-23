acb272f3367ba07c6c46f73f5d844ae112fadfad
AhWeiHENGuEi <aa9440307@gmail.com>
2024-11-23 14:23:40 +0800

    feat: implement insert, remove, and access functions for linked list with corresponding tests

--- src/chapter_array_and_linkedlist/linkedlist.test.ts
+++ src/chapter_array_and_linkedlist/linkedlist.test.ts
@@ -1,5 +1,5 @@
 import { describe, expect, it } from "vitest";
-import { ListNode } from "./linkedlist";
+import { ListNode, access, insert, remove } from "./linkedlist";
 
 describe("Linked List Algorithms", () => {
   it("should return a number from the linked list", () => {
@@ -18,5 +18,52 @@ describe("Linked List Algorithms", () => {
     expect(n2.next).toBe(n3);
     expect(n3.next).toBe(n4);
     expect(n4.next).toBeNull();
+  });
+
+  it("should insert a number into the linked list", () => {
+    const n0 = new ListNode(1);
+    const n1 = new ListNode(3);
+    const n2 = new ListNode(2);
+    const n3 = new ListNode(5);
+    const n4 = new ListNode(4);
+    n0.next = n1;
+    n1.next = n2;
+    n2.next = n3;
+    n3.next = n4;
+
+    insert(n0, 6);
+    expect(n4.next).not.toBeNull();
+    expect(n4.next?.val).toBe(6);
+  });
+
+  it("should remove a number from the linked list", () => {
+    const n0 = new ListNode(1);
+    const n1 = new ListNode(3);
+    const n2 = new ListNode(2);
+    const n3 = new ListNode(5);
+    const n4 = new ListNode(4);
+    n0.next = n1;
+    n1.next = n2;
+    n2.next = n3;
+    n3.next = n4;
+
+    remove(n0, 2);
+    expect(n1.next).toBe(n3);
+  });
+
+  it("should return a number from the linked list", () => {
+    const n0 = new ListNode(1);
+    const n1 = new ListNode(3);
+    const n2 = new ListNode(2);
+    const n3 = new ListNode(5);
+    const n4 = new ListNode(4);
+    n0.next = n1;
+    n1.next = n2;
+    n2.next = n3;
+    n3.next = n4;
+
+    expect(access(n0, 2)).toBe(2);
+    expect(access(n0, 3)).toBe(5);
+    expect(access(n0, 5)).toBeUndefined();
   });
 });
--- src/chapter_array_and_linkedlist/linkedlist.ts
+++ src/chapter_array_and_linkedlist/linkedlist.ts
@@ -6,3 +6,33 @@ export class ListNode {
     this.next = next;
   }
 }
+
+export function insert(node: ListNode, val: number): void {
+  let current = node;
+  while (current.next !== null) {
+    current = current.next;
+  }
+  current.next = new ListNode(val);
+}
+
+export function remove(node: ListNode, val: number): void {
+  let current = node;
+  while (current.next !== null) {
+    if (current.next.val === val) {
+      current.next = current.next.next;
+      return;
+    }
+    current = current.next;
+  }
+}
+
+export function access(node: ListNode, index: number): number | undefined {
+  let current = node;
+  for (let i = 0; i < index; i++) {
+    if (current.next === null) {
+      return undefined;
+    }
+    current = current.next;
+  }
+  return current.val;
+}
